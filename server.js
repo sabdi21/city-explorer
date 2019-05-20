6d1df19595b8fc6cb17bb42db82e6c0000599e58
sabdi21 <sabdi21@hotmail.com>
2019-05-19 17:53:13 -0700

    lab 8 done

--- server.js
+++ server.js
@@ -7,44 +7,84 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+const pg = require('pg');
 
 // Application Setup
 const PORT = process.env.PORT || 3000;
 const app = express();
+const client = new pg.Client(process.env.DATABASE_URL); 
+client.connect();
+client.on('error', err => console.error(err))
+
 app.use(cors());
 
-// API Routes
-app.get('/location', searchToLatLong);
-app.get('/weather', getWeather);
-app.get('/events', getEvents);
-
-//error handler
+//Error handler
 function handleError(err, res) {
-   console.error(err);
-   if (res) res.status(500).send('Sorry, something went wrong');
+  console.error(err);
+  if (res) res.status(500).send('Sorry, something went wrong');
 }
 
-// Helper Functions
-function searchToLatLong(request, response){
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
-    const locationQuery = request.query.data;
+//API Routes 
+app.get('/weather', getWeatherFromSQL);
+app.get('/events', getEvents);
+app.get('/location', getLocationFromSQL);
 
-    superagent.get(url).
-    then(apiResoponse => {
-       const location = new Location(locationQuery, apiResoponse.body)
-       response.send(location)
-    })
-   .catch(error => handlError(error, response));
+
+//Constructor Functions
+function Location(query, data) {
+  this.search_query = query;
+  this.formatted_query = data.formatted_address;
+  this.latitude = data.geometry.location.lat;
+  this.longitude = data.geometry.location.lng;
+ }
+
+ function Weather(day) {
+  this.forecast = day.summary;
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
+ }
+
+ function Event(event) {
+  this.link = event.url;
+  this.name = event.name.text;
+  this.event_date = new Date(event.start.local).toString().slice(0, 15);
+  this.summary = event.summary
 }
 
-function Location(locationQuery, locationInfo) {
- this.search_query = locationQuery;
- this.formatted_query = locationInfo.results[0].formatted_address;
- this.latitude = locationInfo.results[0].geometry.location.lat;
- this.longitude = locationInfo.results[0].geometry.location.lng;
+//HELPER Function
+function getLocationFromSQL(request, response) {
+  const query = request.query.data;
+  const SQL = `SELECT * FROM locations WHERE search_query='${query}';`;
+  return client.query(SQL)
+      .then(result => {
+          if (result.rowCount > 0) {
+              console.log('GETTING LOCATION FROM SQL');
+              // let sqlLocation = new Location(query, result.rows[0]);
+              response.send(result.rows[0]);
+          } else {
+              fetchLocationFromApi(query, response);
+          }
+      })
+      .catch(error => handleError(error, response));
 }
 
-function getWeather(request, response) {
+function fetchLocationFromApi(query, response) {
+  const _URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+  return superagent.get(_URL)
+      .then(data => {
+          console.log('GETTING LOCATION FROM API');
+          if (!data.body.results.length) { throw 'No Data'; }
+          else {
+              const location = new Location(query, data.body.results[0]);
+              const NEWSQL = `INSERT INTO locations (search_query,formatted_query,latitude,longitude) VALUES($1,$2,$3,$4);`;
+              const newValues = Object.values(location);
+              return client.query(NEWSQL, newValues)
+                  .then(() => response.send(location))
+          }
+      });
+}
+
+
+function getWeatherFromSQL(request, response) {
   console.log(request.query.data.formatted_query)
    const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
 
@@ -59,34 +99,55 @@ function getWeather(request, response) {
      .catch(error => handleError(error, response));
 }
 
-function Weather(day) {
- this.forecast = day.summary;
- this.time = new Date(day.time * 1000).toString().slice(0, 15);
+function fetchWeatherFromApi(request, response) {
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+
+  superagent.get(url)
+    .then(apiResponse => {
+        const dailyWeather = apiResponse.body.daily.data.map(day => new Weather(day));
+          const SQL = `INSERT INTO weathers (forecast,time,location_id) 
+                      VALUES ('${dailyWeather.forecast}','${dailyWeather.time}',${request.query.data.id});`;
+          client.query(SQL);
+          response.send(dailyWeather);
+
+      })
+      .catch(error => handleError(error));
+
 }
 
 function getEvents(request, response) {
-    const url = `https://www.eventbriteapi.com/v3/events/search?token=${process.env.EVENTBRITE_API_KEY}&location.address=${request.query.data.formatted_query}`;
+  const url = `https://www.eventbriteapi.com/v3/events/search?token=${process.env.EVENTBRITE_API_KEY}&location.address=${request.query.data.formatted_query}`;
 
-    return superagent.get(url)
-    .then(result => {
-        const events = result.body.events.map(eventData => {
-          console.log("Start monkey data");
-          console.log(eventData);
-            const event = new Event(eventData);
-            return event;
-        });
-        response.send(events);
+  superagent.get(url)
+      .then(result => {
+          const events = result.body.events.map(eventData => {
+              const event = new Event(eventData);
+              return event;
+          });
+
+          response.send(events);
+      })
+      .catch(error => handleError(error, response));
+}
+
+// STATIC METHOD: Fetch location from google
+Location.lookupLocation = (handler) => {
+  const SQL = `SELECT * FROM locations WHERE search_query=$1`;
+  const values = [handler.query];
+
+  return client.query( SQL, values )
+    .then( results => {
+      if( results.rowCount > 0 ) {
+        handler.cacheHit(results);
+      }
+      else {
+        handler.cacheMiss();
+      }
     })
-    .catch(error => handleError(error, response));
-}
-
-function Event(eventData) {
-    this.link = eventData.url;
-    this.name = eventData.name.text;
-    this.event_date = eventData.something;
-    this.summary = eventData.summary
-}
+    .catch( console.error );
+};
 
 
 // Make sure the server is listening for requests
 app.listen(PORT, () => console.log(`App is up on ${PORT}`));
+
